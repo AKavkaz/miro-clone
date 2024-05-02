e473b846cf170497c4144ba65235c2216e08c521
AKavkaz <andrewkhomoutov@hotmail.com>
2024-05-02 17:18:19 +0800

    Organization Bar Changes

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
+import { Hint } from "@/components/hint";
 import { cn } from "@/lib/utils";
 
 interface ItemProps {
@@ -25,16 +26,18 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   return (
     <div className="aspect-square relative">
-      <Image
-        fill
-        src={imageUrl}
-        alt={name}
-        onClick={onClick}
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          fill
+          src={imageUrl}
+          alt={name}
+          onClick={onClick}
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
