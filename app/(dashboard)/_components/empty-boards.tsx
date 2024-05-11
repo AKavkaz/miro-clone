20c81912dce305ba7929f2f77f72cd3d368f4197
AKavkaz <andrewkhomoutov@hotmail.com>
2024-05-11 15:45:28 +0800

    Added search functionality

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -6,9 +6,11 @@ import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
 
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -21,6 +23,7 @@ export const EmptyBoards = () => {
     })
     .then((id) => {
       toast.success("Board created")
+      router.push(`/board/${id}`);
     })
     .catch(() => toast.error("Failed to create board"))
   }
