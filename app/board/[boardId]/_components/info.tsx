df865413156d0bac4a4ab46dbbab7384bfd6666e
AKavkaz <andrewkhomoutov@hotmail.com>
2024-05-19 12:39:21 +0800

    Added room info, participants and toolbar

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -7,6 +7,11 @@ import { Poppins } from "next/font/google";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
 
 interface InfoProps {
   boardId: string;
@@ -17,7 +22,13 @@ const font = Poppins({
   weight: ["600"],
 });
 
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>;
+};
+
 export const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
   const data = useQuery(api.board.get, {
     id: boardId as Id<"boards">,
   });
@@ -26,15 +37,41 @@ export const Info = ({ boardId }: InfoProps) => {
 
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      <Button variant="board" className="px-2">
-        <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
-        <span className={cn(
-          "font-semibold text-xl ml-2 text-black",
-          font.className,
-        )}>
-          Board
-        </span>
-      </Button>
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant="board" className="px-2">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          className="text-base font-normal px-2"
+          onClick={() => onOpen(data._id, data.title)}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom">
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
